295dbab48ea2eb262fd976a473ce6ec0700763a3
Steven <sc201789@ucf.edu>
2025-02-28 12:38:54 -0500

    first commit, frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+//DONE
+import React from 'react';
+import './App.css';
+import { AppProvider } from './components/appContext.js';
+import Layout from './pages/Layout.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return(
+    <div>
+      <BrowserRouter>
+        <AppProvider>
+          <Layout />
+        </AppProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
